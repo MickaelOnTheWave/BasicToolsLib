110b44b05514c3b8c8ec315b87528564f544f210
Mickaël C. Guimarães <mickael.dacruz@gmail.com>
2025-08-03 18:24:47 -0300

    Added FileTools::RemoveFolder() tests

--- tests/src/filetoolstests.cpp
+++ tests/src/filetoolstests.cpp
@@ -1,6 +1,6 @@
 /*
  * BasicToolsLib
- * Copyright (C) 2023 Guimarães Tecnologia Ltda
+ * Copyright (C) 2025 Guimarães Tecnologia Ltda
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -21,6 +21,24 @@
 #include "filetools.h"
 
 using namespace std;
+
+/*******************/
+
+namespace  {
+   void CreatePopulatedFolder(const wstring& folderName)
+   {
+      if (FileTools::FolderExists(folderName))
+         FileTools::RemoveFolder(folderName, false);
+
+      bool ok = FileTools::CreateFolder(folderName);
+      REQUIRE(ok == true);
+
+      const wstring dummyFile = folderName + L"/dummyFile.txt";
+      const wstring dummyContent = L"Lalalala";
+      ok = FileTools::WriteBufferToFile(dummyFile, dummyContent);
+      REQUIRE(ok == true);
+   }
+}
 
 /*******************/
 
@@ -78,5 +96,55 @@ TEST_CASE("FileTools - RemoveFile() - Existing file")
    REQUIRE(ok == true);
 
    const bool exists = FileTools::FileExists(testFilename);
+   REQUIRE(exists == false);
+}
+
+TEST_CASE("FileTools - RemoveFolder() - Inexistent folder")
+{
+   const wstring inexistentFolder = L"hahaha";
+   bool ok = FileTools::RemoveFolder(inexistentFolder, true);
+   REQUIRE(ok == false);
+
+   ok = FileTools::RemoveFolder(inexistentFolder, false);
+   REQUIRE(ok == false);
+}
+
+TEST_CASE("FileTools - RemoveFolder() - Invalid folder")
+{
+   const wstring testFile = L"testFile.txt";
+   FileTools::WriteBufferToFile(testFile, L"My testing content\n\nOn Multiple Lines");
+
+   bool ok = FileTools::RemoveFolder(testFile, true);
+   REQUIRE(ok == false);
+
+   ok = FileTools::RemoveFolder(testFile, false);
+   REQUIRE(ok == false);
+}
+
+TEST_CASE("FileTools - RemoveFolder() - With root")
+{
+   const wstring testFolder = L"testFolder";
+   CreatePopulatedFolder(testFolder);
+
+   const bool ok = FileTools::RemoveFolder(testFolder, false);
+   REQUIRE(ok == true);
+
+   const bool exists = FileTools::FolderExists(testFolder);
+   REQUIRE(exists == false);
+}
+
+TEST_CASE("FileTools - RemoveFolder() - Without root")
+{
+   const wstring testFolder = L"testFolder";
+   CreatePopulatedFolder(testFolder);
+
+   const bool ok = FileTools::RemoveFolder(testFolder, true);
+   REQUIRE(ok == true);
+
+   bool exists = FileTools::FolderExists(testFolder);
+   REQUIRE(exists == true);
+
+   const wstring dummyFile = testFolder + L"/dummyFile.txt";
+   exists = FileTools::FolderExists(dummyFile);
    REQUIRE(exists == false);
 }
